7b74dba951461628f9207cb547f084d790a08feb
Thomas "Pete" Petrovic <pete@freakzero.com>
2017-10-12 12:12:01 +0200

    Getting diff for file to work

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const {getFileHistory} = require('./helpers/getHistory');
+const {getContents} = require('./helpers/getDiff');
 const path = require('path');
 
 function activate(context) {
@@ -7,31 +8,31 @@ function activate(context) {
     constructor() {
       this._onDidChange = new vscode.EventEmitter();
     }
-
+    
     getAsset(assetPath) {
       return vscode.Uri.file(path.join(__dirname, assetPath)).toString();
     }
-
-    provideTextDocumentContent(uri) {  
+    
+    provideTextDocumentContent(uri) { 
       const history = getFileHistory(vscode.window.activeTextEditor.document.uri.fsPath);
       return this.generateChartView(history.getChartDataset());
     }
     
     generateChartView(data) {
       return `
-        <head>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.js"></script>
-          <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-          <script src="${this.getAsset('chart/chart.js')}"></script>
-          <script>
-            $(document).ready(function() {
-              createChart(${JSON.stringify(data)});
-            });
-          </script>
-        </head>
-        <body>
-        <canvas id="timeMachineChart" width="800" height="350"></canvas>
-        </body>
+      <head>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.js"></script>
+      <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+      <script src="${this.getAsset('chart/chart.js')}"></script>
+      <script>
+      $(document).ready(function() {
+        createChart(${JSON.stringify(data)});
+      });
+      </script>
+      </head>
+      <body>
+      <canvas id="timeMachineChart" width="800" height="350"></canvas>
+      </body>
       `
     }
     get onDidChange() {
@@ -44,16 +45,23 @@ function activate(context) {
   
   let preview = vscode.Uri.parse('time-machine://authority/time-machine');
   let provider = new GitWindowContentProvider();
-
+  
   vscode.workspace.registerTextDocumentContentProvider('time-machine', provider);
   
   vscode.commands.registerCommand('extension.didClick', function (data) {
-    return vscode.commands.executeCommand('vscode.diff', vscode.window.visibleTextEditors[0].document.uri, vscode.window.visibleTextEditors[0].document.uri, 'some diff').then((success) => {
-    }, function(err) {
-      vscode.window.showInformationMessage(err.message);    
-    });
+    getContents(
+      'src/lib/output.js',
+      '4677d5630db5761eba6580232b63c661eb02e910',
+      '29f8c9ead90a610931da14f68715a75222f1f004'
+    ).then(documents => {
+      return vscode.commands.executeCommand('vscode.diff', documents.left.uri, documents.right.uri, 'some diff').then((success) => {
+      }, function(err) {
+        console.log(err);
+        vscode.window.showInformationMessage(err.message);    
+      });  
+    })
   });
-
+  
   let disposable = vscode.commands.registerCommand('extension.vscodeTimeMachine', function () {
     return vscode.commands.executeCommand('vscode.previewHtml', preview, vscode.ViewColumn.Two, 'Time Machine').then((success) => {
     }, function(err) {
@@ -69,7 +77,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
---
+++ helpers/getDiff.js
@@ -0,0 +1,67 @@
+const vscode = require('vscode');
+const {spawn} = require('child_process');
+
+function gitRunner(gitCmdArray) {
+  
+  return new Promise((resolve, reject) => {  
+    let output = [];
+    let commandBase = ['-C', vscode.workspace.rootPath];
+    
+    const ps = spawn('git', commandBase.concat(gitCmdArray));
+    ps.stdout.on('data', (data) => {
+      output.push(data);
+    });
+    
+    // ps.stderr.on('err', (data) => {
+    
+    // });
+    
+    // reject if fail
+    ps.on('close', (code) => {
+      resolve(output.join('/n'));
+    });
+  })
+}
+
+function getContents(file, hashLeft, hashRight) {
+  return new Promise((resolve, reject) => {
+    gitRunner(['show', `${hashLeft}:${file}`]).then(contentLeft => {
+      gitRunner(['show', `${hashRight}:${file}`]).then(contentRight => {
+        vscode.workspace.openTextDocument({
+          language: 'javascript', 
+          content: contentLeft
+        }).then(left => {
+          vscode.workspace.openTextDocument({
+            language: 'javascript', 
+            content: contentRight
+          }).then(right => {
+            resolve({
+              left, 
+              right
+            });
+          });
+        })    
+      });
+    });
+  })
+}
+
+// return vscode.commands.executeCommand('vscode.diff', doc.uri, doc2.uri, 'some diff').then((success) => {
+// }, function(err) {
+//   console.log(err);
+//   vscode.window.showInformationMessage(err.message);    
+// });
+
+// function getDocuments(contentLeft, contentRight) {
+//   const diffBuffLeft = vscode.workspace.openTextDocument({
+//     language: 'javascript', 
+//     content: 'i will bang your mom'
+//   });
+
+//   const diffBuffRight = vscode.workspace.openTextDocument({
+//     language: 'javascript', 
+//     content: 'i will bang your mom'
+//   });
+// }
+
+exports.getContents = getContents;
